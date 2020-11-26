72f57638df3789de7c729bd291c146e9cdf0b3d7
tejan-singh <tstejan@gmail.com>
2020-11-26 21:47:04 +0530

    add playSound() function to play sound on mouse click

--- game.js
+++ game.js
@@ -13,10 +13,17 @@ function nextSequence(){
 
   $("#" + randomChosenColour).fadeOut(100).fadeIn(100)
 
-  var sound = new Audio("sounds/" + randomChosenColour + ".mp3")
-  sound.play()
-
-  return randomChosenColour
 }
 
   $(".btn").on("click", function(){userChosenColour = this.id; console.log(userChosenColour); userClickedPattern.push(userChosenColour); console.log(userClickedPattern)})
+
+  
+  function playSound(){
+    $(".btn").on("click", function(){
+      userChosenColour = this.id; 
+      var sound = new Audio("sounds/" + userChosenColour + ".mp3")
+      sound.play()
+    }
+    )
+
+  }
