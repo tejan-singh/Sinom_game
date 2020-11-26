43501fb3865c4b9b67b48050796e56edab7461dc
tejan-singh <tstejan@gmail.com>
2020-11-26 21:28:56 +0530

    add sound feature into nextSequence() function

--- game.js
+++ game.js
@@ -13,14 +13,10 @@ function nextSequence(){
 
   $("#" + randomChosenColour).fadeOut(100).fadeIn(100)
 
+  var sound = new Audio("sounds/" + randomChosenColour + ".mp3")
+  sound.play()
+
   return randomChosenColour
 }
 
-function makeSound(){
-    var sound = new Audio("sounds/" + nextSequence() + ".mp3")
-    sound.play()
-  }
-
   $(".btn").on("click", function(){userChosenColour = this.id; console.log(userChosenColour); userClickedPattern.push(userChosenColour); console.log(userClickedPattern)})
-
-  //userChosenColour = this.id; userClickedPattern.push(userChosenColour)
