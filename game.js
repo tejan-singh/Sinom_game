775aa9c97897b9e5650f1080f39609b537e6482a
tejan-singh <tstejan@gmail.com>
2020-11-28 23:38:46 +0530

    add checkAnswer() function to check answers only on console

--- game.js
+++ game.js
@@ -5,20 +5,14 @@ userClickedPattern = []
 game_start = 0
 level = 0
 
-$(document).keypress(function(press){
-  if(game_start === 0){
-    $("h1").html("Level 0")
-    nextSequence()
-    game_start = 1
-  }
-})
+function nextSequence() {
+  playSound()
+  animatePress()
 
-function nextSequence(){
   level = level + 1
   $("h1").html("Level " + level)
   randomNumber = Math.random()
   randomNumber = Math.round(randomNumber * 3)
-  console.log(randomNumber)
 
   randomChosenColour = buttonColours[randomNumber]
   gamePattern.push(randomChosenColour)
@@ -31,30 +25,61 @@ function nextSequence(){
 
 }
 
-  //$(".btn").on("click", function(){userChosenColour = this.id; console.log(userChosenColour); userClickedPattern.push(userChosenColour); console.log(userClickedPattern)})
+function playSound() {
+  $(".btn").on("click", function () {
+    userChosenColour = this.id
+    var sound = new Audio("sounds/" + userChosenColour + ".mp3")
+    sound.play()
+    $("#" + userChosenColour).fadeOut(100).fadeIn(100)
+  })
 
-  
-  function playSound(){
-    $(".btn").on("click", function(){
-      userChosenColour = this.id 
-      var sound = new Audio("sounds/" + userChosenColour + ".mp3")
-      sound.play()
-      $("#" + userChosenColour).fadeOut(100).fadeIn(100)
-    }
-    )
+}
 
-  }
+function animatePress() {
+  $(".btn").on("click", function () {
+    pressed = this.id
 
-  function animatePress(){
-    $(".btn").on("click", function(){
-      pressed = this.id
-      
-      $("#" + pressed).addClass("pressed")
-      
-      setTimeout(function() {
+    $("#" + pressed).addClass("pressed")
+
+    setTimeout(function () {
       $("#" + pressed).removeClass("pressed")
-      }, 100);
-      
-    })
+    }, 100);
+
+  })
+}
+
+function checkAnswer(gamePattern, userClickedPattern) {
+  result = JSON.stringify(gamePattern) == JSON.stringify(userClickedPattern)
+
+  if (result) {
+    console.log("Match")
+    userClickedPattern.length = 0;
+
+    setTimeout(function () {
+      nextSequence()
+    }, 1000);
+    
+  } else {
+    console.log("No match")
   }
+
+}
+
+$(document).keypress(function (press) {
+  if (game_start === 0) {
+    $("h1").html("Level 0")
+    nextSequence()
+    game_start = 1
+  }
+})
+
+$(".btn").on("click", function () {
+  userChosenColour = this.id;
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern)
+  checkAnswer(gamePattern, userClickedPattern)
+})
+
+
+
 
